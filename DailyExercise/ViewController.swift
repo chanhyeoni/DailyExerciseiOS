0033ec5de93e4508e0ff346f789236602f296362
Changhyun Lee <chlee021690@changhyuns-macbook-air.local>
2016-06-21 23:08:23 -0400

    Initial Commit

---
+++ DailyExercise/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DailyExercise
+//
+//  Created by Changhyun Lee on 6/21/16.
+//  Copyright © 2016 Changhyun Lee. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
