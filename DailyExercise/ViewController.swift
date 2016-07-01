31bb6f3c28803a2319abaf971d8c7c340b93fd99
Changhyun Lee <chlee021690@Changhyuns-MacBook-Air.local>
2016-06-30 20:21:39 -0400

    initial commit

--- DailyExercise/ViewController.swift
+++ DailyExercise/ViewController.swift
@@ -8,7 +8,16 @@
 
 import UIKit
 
+
 class ViewController: UIViewController {
+    
+    
+    // MARK: Properties
+    
+    @IBOutlet weak var username: UITextField!
+    @IBOutlet weak var password: UITextField!
+    
+    
 
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -19,6 +28,13 @@ class ViewController: UIViewController {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    
+    @IBAction func login(sender: UIButton) {
+        
+        
+    }
+    
 
 
 }
